b4791bb6a8efb4f4638b3f92ded0ac09f6dd2094
Thomas Seleiro <thomas.seleiro@gmail.com>
2023-05-11 16:17:09 +0100

    Update extension button to skip ad

--- background.js
+++ background.js
@@ -1,5 +1,7 @@
-const alertFunc = () => {
-  alert("You have run the extension code");
+const skipVideo = () => {
+  document
+    .querySelector(".ytp-ad-skip-button-slot")
+    .dispatchEvent(new Event("click"));
 };
 
 const ytVideoUrl = "https://www.youtube.com/watch";
@@ -8,7 +10,7 @@ chrome.action.onClicked.addListener(async (tab) => {
   if (tab.url.startsWith(ytVideoUrl)) {
     await chrome.scripting.executeScript({
       target: { tabId: tab.id },
-      func: alertFunc,
+      func: skipVideo,
     });
   }
 });
